679a33b99e9e431fee476c8616549e6bf8d4c30b
NishithD <nishithdasari100@gmail.com>
2023-02-17 21:23:58 +0530

    ajax

--- login.js
+++ login.js
@@ -1,34 +1,62 @@
 const mysql = require('mysql2');
-
-function submitLoginForm(event) {
+async function submitLoginForm(event) {
   event.preventDefault();
 
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+  const form = document.getElementById('login-form');
+  const formData = new FormData(form);
 
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'nishith',
-    password: '152128',
-    database: 'login_page'
+  const response = await fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: formData.get('username'),
+      password: formData.get('password')
+    })
   });
 
-  connection.query(
-    'SELECT * FROM users WHERE username = ? AND password = ?',
-    [username, password],
-    (error, results, fields) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
+  const { success } = await response.json();
 
-      if (results.length > 0) {
-        alert('Login successful!');
-      } else {
-        alert('Incorrect username or password.');
-      }
-    }
-  );
+  if (success) {
+    window.location.href = 'success.html';
+  } else {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = 'Invalid username or password';
+  }
 }
 
+
+// function submitLoginForm(event) {
+//   event.preventDefault();
+
+//   const username = document.getElementById('username').value;
+//   const password = document.getElementById('password').value;
+
+//   const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'nishith',
+//     password: '152128',
+//     database: 'login_page'
+//   });
+
+//   connection.query(
+//     'SELECT * FROM users WHERE username = ? AND password = ?',
+//     [username, password],
+//     (error, results, fields) => {
+//       if (error) {
+//         console.error(error);
+//         return;
+//       }
+
+//       if (results.length > 0) {
+//         alert('Login successful!');
+//       } else {
+//         alert('Incorrect username or password.');
+//       }
+//     }
+//   );
+// }
+
 document.getElementById('loginForm').addEventListener('submit', submitLoginForm);
+
