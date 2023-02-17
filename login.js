acaaffc9f736ea58b7c449a1049bb516efce0c1b
NishithD <nishithdasari100@gmail.com>
2023-02-17 21:50:59 +0530

    final

--- login.js
+++ login.js
@@ -1,51 +1,31 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
+const connection = await mysql.createConnection({
     host: 'localhost',
-    user: 'nishith',
-    password: '152128',
-    database: 'login_page'
+    port: '8080',
+    user: '<nishith>',
+    password: '<152128>',
+    database: '<login_page.sql>'
 });
 
 function authenticateUser(username, password) {
-    return new Promise((resolve, reject) => {
-        const sql = 'SELECT * FROM users WHERE username = ?';
-        connection.query(sql, [username], (error, results, fields) => {
-            if (error) {
-                reject(error);
-            } else {
-                if (results.length > 0) {
-                    const user = results[0];
-                    if (user.password === password) {
-                        resolve({ success: true, username: user.username });
-                    } else {
-                        resolve({ success: false, message: "Incorrect password" });
-                    }
+    return new Promise(async (resolve, reject) => {
+        try {
+            const [rows, fields] = await connection.execute('SELECT * FROM users WHERE username = ?', [username]);
+
+            if (rows.length > 0) {
+                const user = rows[0];
+
+                if (user.password === password) {
+                    resolve({ success: true, username: user.username });
                 } else {
-                    resolve({ success: false, message: "Username not found" });
+                    resolve({ success: false, message: 'Incorrect password' });
                 }
+            } else {
+                resolve({ success: false, message: 'Username not found' });
             }
-        });
+        } catch (err) {
+            reject(err);
+        }
     });
 }
-
-const form = document.getElementById('login-form');
-
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const username = form.elements['username'].value;
-    const password = form.elements['password'].value;
-
-    try {
-        const result = await authenticateUser(username, password);
-        if (result.success) {
-            window.location.href = 'success.html';
-        } else {
-            alert(result.message);
-        }
-    } catch (error) {
-        console.error(error);
-        alert('An error occurred while trying to authenticate your login.');
-    }
-});
